ba1c80c056eeaea25de448a306368463d257bd4a
Seongmin Oh <saogig0503@naver.com>
2026-01-14 17:51:44 +0900

    Initial Commit

---
+++ src/main/java/com/example/playmon_server/PlaymonServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.playmon_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaymonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaymonServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/playmon_server/PlaymonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playmon_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaymonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
